28c2bb8978f7f36b672be43096fb85073b8a3f28
oguzhan <oguzhanngoley@gmail.com>
2025-11-29 04:39:43 +0300

    18 finished

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,11 @@
+﻿global using Basket.API.Models;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using BuildingBlocks.CQRS;
+global using FluentValidation;
+global using BuildingBlocks.Behaviours;
+global using BuildingBlocks.Exceptions.Handler;
+global using BuildingBlocks.Exceptions;
+global using Basket.API.Data;
+
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System.Reflection.Metadata;
+
+namespace Catalog.API.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description,string ImageFile,decimal Price):ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductCommanValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommanValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greated than 0");
+        }
+    }
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Name = command.Name,
+                Price = command.Price,
+            };
+
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
